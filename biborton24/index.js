726f59bc8d7af0256abd4d5765272d637fea0d08
d3vi1 <hannanhridoy@gmail.com>
2020-11-28 02:23:16 +0600

    Added js support and nice tri categorise design

---
+++ biborton24/index.js
@@ -0,0 +1,6 @@
+let e1 = document.getElementById('e1')
+let l1 = document.getElementById('l1')
+let en1 = document.getElementById('en1')
+e1.style.display = 'block'
+l1.style.display = 'block'
+en1.style.display = 'block'
