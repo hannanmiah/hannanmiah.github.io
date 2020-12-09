26dba825a8a22d01acf37710b05ac62812e94723
d3vi1 <hannanhridoy@gmail.com>
2020-12-10 02:34:03 +0600

    Added nice sidebar menu and made it responsive

--- biborton24/pages/category/index.js
+++ biborton24/pages/category/index.js
@@ -1,3 +1,15 @@
 let n1 = document.getElementById('n1')
+let sidebar = document.querySelector('aside')
+let sidebarClose = document.getElementById('sidebarClose')
+let sidebarOpen = document.getElementById('sidebarOpen')
 
+//make first content of post visible
 n1.style.display = 'block'
+//toggling between sidebar and menu
+sidebarClose.addEventListener('click', function () {
+    sidebar.style.display = 'none'
+})
+
+sidebarOpen.addEventListener('click', function () {
+    sidebar.style.display = 'block'
+})
--- biborton24/pages/post-details/index.js
+++ biborton24/pages/post-details/index.js
@@ -1,14 +1,7 @@
-let e1 = document.getElementById('e1')
-let l1 = document.getElementById('l1')
-let en1 = document.getElementById('en1')
 let sidebar = document.querySelector('aside')
 let sidebarClose = document.getElementById('sidebarClose')
 let sidebarOpen = document.getElementById('sidebarOpen')
 
-//displaying first image of every category
-e1.style.display = 'block'
-l1.style.display = 'block'
-en1.style.display = 'block'
 //toggling between sidebar and menu
 sidebarClose.addEventListener('click', function () {
     sidebar.style.display = 'none'
