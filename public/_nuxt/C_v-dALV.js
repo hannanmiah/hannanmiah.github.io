10cd09c130a9a0c1b916685816fb99e9758eacee
Hannan Miah <hannanhridoy@gmail.com>
2025-11-23 20:56:17 +0600

    chore: new deploy

---
+++ public/_nuxt/C_v-dALV.js
@@ -0,0 +1,1 @@
+import{r as n}from"./a_h5BOEm.js";import a from"./9g-8mTbO.js";import{a1 as e,b as s,o as t,w as i,e as p}from"./Bx__7DS1.js";import"./iik6CYzq.js";const d={__name:"ProseWarning",setup(c){const o=e();return(r,f)=>(t(),s(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
